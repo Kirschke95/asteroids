41da2c5fe51b9166ece54bd1ac84277e6fe9d809
Kirschke95 <tylerkirschke95@gmail.com>
2025-02-14 11:55:57 -0500

    player can now move forward and rotate

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-#this allows us to use code from
-#the open-source pygame library
-#throughout this file
 import pygame
 from constants import *
-from player import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -19,6 +16,7 @@ def main():
 
         screen.fill("black")
         player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
 
         #limit framerate to 60 fps
