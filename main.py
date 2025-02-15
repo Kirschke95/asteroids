8536b70a08c86e51e3e9654306b39ab33ec57e3e
Kirschke95 <tylerkirschke95@gmail.com>
2025-02-15 13:23:55 -0500

    asteroid field now spawns, collision detection works, and the player can shoot

--- main.py
+++ main.py
@@ -1,22 +1,51 @@
-import pygame
+import sys
+import pygame # type: ignore
 from constants import *
 from player import Player
+from asteroidfield import *
+from shot import Shot
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
+    #groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    
+   
+    
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    AsteroidField.containers = (updatable)
+    asteroid_field = AsteroidField()
+    
+    Player.containers = (updatable, drawable)
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    
+    dt = 0 
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        updatable.update(dt)
+        
+        for obj in asteroids:
+            if obj.collision_check(player):
+                print("Game over!")
+                sys.exit()
+        
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
         #limit framerate to 60 fps
